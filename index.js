a60e6971741e438b342d7895eb024fb732c236db
DavidSark <95092241+DavidSark@users.noreply.github.com>
2023-05-29 13:01:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+import path from 'path';
+import express from 'express';
+import cors from 'cors';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, '../dist')))
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api", (req, res) => {
+    res.json({ message: 'Hello world' });
+});
+
+const contactEmail = nodemailer.createTransport({
+    port: 465,               // true for 465, false for other ports
+    host: "mail.infomaniak.com",
+    auth: {
+        user: process.env.EMAIL_ADRESS,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+    secure: true,
+})
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("ready to send")
+    }
+})
+
+
+app.post("/api/contact", bodyParser.urlencoded({ extended: false }), (req, res) => {
+    const name =  req.body.lastName + '  ' + req.body.firstName;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const address = req.body.address;
+    const postalCode = req.body.postalCode;
+    const prestation = req.body.prestation;
+    const message = req.body.message;
+    const mail = {
+        from: `"${name}" <${process.env.EMAIL_ADRESS}>"`,
+        to: process.env.EMAIL_ADRESS,
+        subject: "Contact form submission",
+        html: `<p>Name : ${name}</p>` +
+            `<p>Email : ${email}</p>` +
+            `<p>Phone : ${phone}</p>` +
+            `<p>Address : ${address}</p>` +
+            `<p>Postal : ${postalCode}</p>` +
+            `<p>Prestation : ${prestation}</p>` +
+            `<p>Message : ${message}</p>`
+    };
+    contactEmail.sendMail(mail, (error) => {
+        if (error) {
+            res.json(error);
+        } else {
+            res.json({ code: 200, status: "Message sent" });
+        }
+    });
+});
+
+
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html' ));
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
